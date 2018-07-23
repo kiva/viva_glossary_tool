7a62eec82f61c8c19b1a25aba27132c666d2b79d
Kevin Wang <kevinw@Kiva-NicoleK.local>
2018-07-23 10:49:40 -0700

    chrome extension tooltip and link

--- chrome/content.js
+++ chrome/content.js
@@ -1,6 +1,8 @@
 $(function() {
 	let glossary = {}
 
+	$( document ).tooltip();
+
 	chrome.storage.sync.get('english-glossary', (data) => {
 		fetch(data['english-glossary'])
 			.then((resp) => resp.json())
@@ -11,6 +13,7 @@ $(function() {
 			})
 	})
 
+	// just an example of communication between content script and popup script
 	chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 		    if (request.enabled)
 		      	console.log(glossary)
@@ -22,13 +25,23 @@ $(function() {
 	}
 
 	function highlightTerms() {
-		let descr = $('.loan-description')
-		if (descr.length) {
-			for (let word in glossary) {
-				let re = new RegExp('\\b' + word + '\\b', 'ig')
-				descr.html(descr.html()
-					.replace(re, '<span class="highlight">$&</span>'))
+		// find all words that appear in glossary and wrap them with spans
+		let highlightSections = ['.editorOriginalText', '.loan-description']
+		for (section of highlightSections) {
+			let descr = $(section)
+			if (descr.length) {
+				for (let word in glossary) {
+					let re = new RegExp('\\b' + word + '\\b', 'ig')
+					descr.html(descr.html()
+						.replace(re, '<span class="highlight"><a target="_blank">$&</a></span>'))
+				}
 			}
 		}
+		// add links and tooltip
+		$('.highlight').each(function() {
+			$(this).find('a').attr('href', 'https://kiva.forumbee.com' + glossary[$(this).text()].url)
+			$(this).append(`<span class="viva-tooltip">${glossary[$(this).text()].meaning}</span>`)
+		})
 	}
+
 })
