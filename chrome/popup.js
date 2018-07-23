a9e271ecde4a7ef7a1c101ebb5d494cee168c92a
Kevin Wang <kevinzhiwang@gmail.com>
2018-07-23 13:37:26 -0700

    add search functionality to popup
    not too good looking right now, but I can just add more CSS

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,8 +1,40 @@
 $(function() {
-	$('button').click((e) => {
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, {enabled: true});
-		});
+	let glossary = {}
+	
+	chrome.storage.sync.get('english-glossary', (data) => {
+	fetch(data['english-glossary'])
+		.then((resp) => resp.json())
+		.then((json) => {
+			glossary = json
+			for (let country in glossary) {
+				if (country !== '')
+					$('#countries').append(`<option value="${country}">${country}</option>`)
+			}
+		})
 	})
+
+	$('#search-input').on('input', displayTerm)
+	$('#countries').on('change', displayTerm)
+
+	function displayTerm() {
+		let term = $('#search-input').val().toLowerCase()
+		let data = getTerm(term)
+		if (data) {
+			$('#results').html(`${data.meaning}<br><a target="_blank" class="forumbee-link" href="https://kiva.forumbee.com${data.url}">Read more</a>`)
+			$('#results').addClass('definition')
+		} else {
+			$('#results').text('The definition will appear here.')
+			$('#results').removeClass('definition')
+		}
+	}
+
+	function getTerm(term) {
+		let country = $('#countries').val()
+		if (glossary[country][term] !== undefined) {
+			return glossary[country][term]
+		} else {
+			return glossary[''][term]
+		}
+	}
 })
 
